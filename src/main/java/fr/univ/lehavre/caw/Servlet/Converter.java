590e93ee72002972d5f41da978d41ce01441403e
jeremy27 <courel.jeremy@gmail.com>
2014-02-02 15:46:53 +0100

    correction warning + html + indentation

--- src/main/java/fr/univ/lehavre/caw/Servlet/Converter.java
+++ src/main/java/fr/univ/lehavre/caw/Servlet/Converter.java
@@ -11,38 +11,47 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Converter
  */
-
 public class Converter extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{	
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TO DO : renvoie sur la page d'accueil
-		
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{	
-		String carac = request.getParameter("caractere");
-		
-		int val =(int)carac.charAt(0);
-		String carac_decimal = new String(val+""); 
-		String carac_hexa = Integer.toString(val, 16);
-		String carac_html = "&#"+val;
-		String carac_unicode = "&0x"+carac_hexa;
+    }
 
-		String s = "{\"carac\":\""+carac+"\",\"decimal\":\""+carac_decimal+"\",\"hexa\":\""+carac_hexa+"\",\"html\":\""+carac_html+"\",\"unicode\":\""+carac_unicode+"\"}";
-	
-		System.out.println(s);
-		
+    /**
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String carac = request.getParameter("caractere");
+
+        int val = (int) carac.charAt(0);
+        String carac_decimal = new String(val + "");
+        String carac_hexa = Integer.toString(val, 16);
+        String carac_html = "&#" + val;
+        String carac_unicode = "&0x" + carac_hexa;
+
+        String s = "{\"carac\":\"" + carac + "\",\"decimal\":\"" + carac_decimal + "\",\"hexa\":\"" + carac_hexa + "\",\"html\":\"" + carac_html + "\",\"unicode\":\"" + carac_unicode + "\"}";
+
+        System.out.println(s);
+
 		// TO DO : renvoie les valeurs converties au client
-		
-	}
+    }
 
 }
